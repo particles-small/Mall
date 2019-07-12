5483f439fbb35d8057ed6f36938a17d901185c0d
particles-small <sr13087513851@163.com>
2019-07-12 21:41:11 +0800

    Updates

---
+++ precache-manifest.363eb1e6d3cd46d13b0ec700e9375d2d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df802f0538c5bec42d6680518d1ca681",
+    "url": "/Mall/index.html"
+  },
+  {
+    "revision": "149fee1b19ecf00eaa3f",
+    "url": "/Mall/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "81dab01e4401d1c99f22",
+    "url": "/Mall/static/css/main.4f113a20.chunk.css"
+  },
+  {
+    "revision": "149fee1b19ecf00eaa3f",
+    "url": "/Mall/static/js/2.6cd716b6.chunk.js"
+  },
+  {
+    "revision": "81dab01e4401d1c99f22",
+    "url": "/Mall/static/js/main.597440a1.chunk.js"
+  },
+  {
+    "revision": "ea3a3f085000279e41d4",
+    "url": "/Mall/static/js/runtime~main.9797cb3a.js"
+  },
+  {
+    "revision": "04b867f85cda6dcb5df998f27ae11a81",
+    "url": "/Mall/static/media/icon1.04b867f8.png"
+  },
+  {
+    "revision": "78e80d984c120e48390138ffbba56f8a",
+    "url": "/Mall/static/media/slide1.78e80d98.jpg"
+  },
+  {
+    "revision": "e568b6f591ffdefd7ca5ab556c051bbd",
+    "url": "/Mall/static/media/slide2.e568b6f5.jpg"
+  },
+  {
+    "revision": "f336ccaba911971174896446cbf2997b",
+    "url": "/Mall/static/media/slide3.f336ccab.jpg"
+  },
+  {
+    "revision": "165ce051678d75fb0741be7f5e38599f",
+    "url": "/Mall/static/media/slide4.165ce051.jpg"
+  },
+  {
+    "revision": "a2a9b0e6a7c659572d99fb0ee41c52a6",
+    "url": "/Mall/static/media/slide5.a2a9b0e6.jpg"
+  },
+  {
+    "revision": "5d188f487477009429d097c3e7051b32",
+    "url": "/Mall/static/media/slide6.5d188f48.jpg"
+  },
+  {
+    "revision": "fd0e6f260d3a21cccac62bae22e3068d",
+    "url": "/Mall/static/media/slide7.fd0e6f26.jpg"
+  },
+  {
+    "revision": "5f02728dda9bae38d55748475706bf30",
+    "url": "/Mall/static/media/slide8.5f02728d.jpg"
+  },
+  {
+    "revision": "68ddc0570016fc3a6817cee807efada7",
+    "url": "/Mall/static/media/图标 25.68ddc057.svg"
+  },
+  {
+    "revision": "44aaf9c0f27a3900f2bf0aded6add67f",
+    "url": "/Mall/static/media/图标 96.44aaf9c0.svg"
+  },
+  {
+    "revision": "10c785cf1d7637947ef00ad57e7192aa",
+    "url": "/Mall/static/media/图标 97.10c785cf.svg"
+  }
+]);
